082f14a494734d3e71250bdf64e2001274e1860c
Brooke Bryan <brooke@bajb.net>
2025-11-28 13:39:56 +0000

    Support catalog default

--- catalog.go
+++ catalog.go
@@ -4,8 +4,14 @@ type Catalog struct {
 	Languages map[string]Translator
 }
 
-func NewCatalog() *Catalog {
+func NewEmptyCatalog() *Catalog {
 	return &Catalog{Languages: make(map[string]Translator)}
+}
+
+func NewCatalog(defaultLang string, defaultTrans Translator) *Catalog {
+	return &Catalog{Languages: map[string]Translator{
+		defaultLang: defaultTrans,
+	}}
 }
 
 func (c *Catalog) AddLanguage(language string, translator Translator) {
@@ -14,4 +20,21 @@ func (c *Catalog) AddLanguage(language string, translator Translator) {
 
 func (c *Catalog) GetTranslator(language string) Translator {
 	return c.Languages[language]
+}
+
+func (c *Catalog) Quick(language string) QuickAdd {
+	return QuickAdd{c: c, lang: language}
+}
+
+type QuickAdd struct {
+	c    *Catalog
+	lang string
+}
+
+func (q QuickAdd) Add(translator interface{}, err error) {
+	if err == nil && translator != nil {
+		if trans, ok := translator.(Translator); ok {
+			q.c.Languages[q.lang] = trans
+		}
+	}
 }
