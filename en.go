784577b14eaf1769ce42dece85402b1404801bd1
Brooke Bryan <brooke@bajb.net>
2025-12-10 12:59:17 +0000

    Force update

--- en.go
+++ en.go
@@ -6,7 +6,7 @@ import (
 	"github.com/packaged/helpers-go"
 )
 
-// ENPlural allows an EN conversion of {QTY} bag(s) full
+// ENPlural allows an EN conversion of {qty} bag(s) full
 func ENPlural(txt string, quantity int64) string {
 	txt = strings.ReplaceAll(txt, "(s)", helpers.If(quantity == 1, "", "s"))
 	return txt
